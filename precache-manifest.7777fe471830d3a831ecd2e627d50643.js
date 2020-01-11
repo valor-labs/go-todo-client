0802aab259c6c0c728d5bca5644ee2ff6cb29462
buchslava <vyacheslav.chub@gmail.com>
2020-01-11 12:52:54 +0200

    Updates

---
+++ precache-manifest.7777fe471830d3a831ecd2e627d50643.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e720500004d9ea664a3c3e27f23f60",
+    "url": "/go-todo-client/index.html"
+  },
+  {
+    "revision": "659548fffe65a87913f2",
+    "url": "/go-todo-client/static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "64bde8980ae08185f790",
+    "url": "/go-todo-client/static/css/main.a3bfdbb4.chunk.css"
+  },
+  {
+    "revision": "659548fffe65a87913f2",
+    "url": "/go-todo-client/static/js/2.81659624.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/go-todo-client/static/js/2.81659624.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64bde8980ae08185f790",
+    "url": "/go-todo-client/static/js/main.456a4f12.chunk.js"
+  },
+  {
+    "revision": "fde5b679f0e996cb1cac",
+    "url": "/go-todo-client/static/js/runtime-main.8d0e730d.js"
+  }
+]);
